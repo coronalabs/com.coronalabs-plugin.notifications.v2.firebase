ba7a7617edc023dbaf6578a260b077aa14278ad3
Vlad Shcherban <vlad@coronalabs.com>
2020-12-27 20:44:04 +0200

    Fixing up the plugin for first release

--- plugins/2020.3569/android/metadata.lua
+++ plugins/2020.3569/android/metadata.lua
@@ -19,13 +19,6 @@ local metadata =
                             <action android:name="com.google.firebase.MESSAGING_EVENT"/>
                         </intent-filter>
                     </service>
-
-                    <service
-                        android:name="plugin.notifications.v2.CoronaFirebaseInstanceIdService">
-                        <intent-filter>
-                            <action android:name="com.google.firebase.INSTANCE_ID_EVENT"/>
-                        </intent-filter>
-                    </service>                    
                 ]]
             }
         }
