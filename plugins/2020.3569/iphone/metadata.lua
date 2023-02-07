6d9dd7fe44d564f22421363243b9c90e2db0f323
Scott Harrison <scottrules44@gmail.com>
2023-02-06 22:15:39 -0600

    iOS: Firebase Lib Update

--- plugins/2020.3569/iphone/metadata.lua
+++ plugins/2020.3569/iphone/metadata.lua
@@ -3,8 +3,8 @@ local metadata =
 	plugin =
 	{
 		format = 'staticLibrary',
-		staticLibs = { "z", "sqlite3", "NotificationsV2Plugin.firebase", "FirebaseMessaging" },
-		frameworks = {'PromisesObjC', 'FirebaseCore', 'FirebaseCoreDiagnostics', 'FirebaseInstallations', 'GoogleDataTransport', 'GoogleUtilities', 'nanopb', "GoogleAppMeasurement", "GoogleAppMeasurementIdentitySupport", "CoreTelephony" },
+		staticLibs = { "z", "sqlite3", "NotificationsV2Plugin.firebase" },
+		frameworks = {'FBLPromises', 'FirebaseCore', 'FirebaseCoreInternal', 'FirebaseInstallations', 'FirebaseMessaging', 'GoogleAppMeasurement', 'GoogleAppMeasurementIdentitySupport', 'GoogleDataTransport', 'nanopb', "GoogleUtilities", "CoreTelephony" },
 		frameworksOptional = { 'UserNotifications', 'UserNotificationsUI' },
 		delegates = { 'CoronaNotificationsDelegate' }
 	}
